f938e9e5d5923f44da4c0c925c6bf1467d07992d
DavidSucias <david.sucias@gft.com>
2021-07-21 11:21:30 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Sucias Colomer, David on 21/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
